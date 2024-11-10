11aed821bcd22a49f48e4cf3c6d0666faf9aaa3f
Eduardo Ribeiro <eduardo20003131@gmail.com>
2024-11-09 21:31:31 -0300

    Layot base das telas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Página inicial
+router.get('/', (req, res) => res.render('index'));
+
+//pagina de cadastro
+router.get('/cadastro', (req, res) => res.render('cadastro'));
+
+// Tela de solicitação
+router.get('/solicitacao', (req, res) => res.render('solicitacao'));
+
+// Perfil do aluno
+router.get('/perfil', (req, res) => res.render('perfil'));
+
+// Página de administração
+router.get('/admin', (req, res) => res.render('admin'));
 
 module.exports = router;
